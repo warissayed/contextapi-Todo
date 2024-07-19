3c8172877b158301aa1a06f8aecf38f0c13f9cbd
waris_sayed <warissayed72@gmail.com>
2024-07-19 16:15:19 +0530

    updated the check mark and h and w  full screen

--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -1,24 +1,21 @@
-import { createContext,useContext } from "react";
+import { createContext, useContext } from "react";
 
- export const TodoContext =createContext({
-
+export const TodoContext = createContext({
   todos: [
     {
-      id:1,
-      todo:"togo msg",
-      completed: false
-    }
-
+      id: 1,
+      todo: "togo msg",
+      completed: false,
+    },
   ],
-  addTodo: (todo) =>{},
-  updateTodo:(id,todo)=>{},
-  deleteTodo:(id)=>{},
-  toggleCompete:(id)=>{}
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleCompete: (id) => {},
+});
 
- })
+export const useTodo = () => {
+  return useContext(TodoContext);
+};
 
- export const useTodo = ()=>{
-  return useContext(TodoContext)
- }
-
- export const TodoProvider = TodoContext.Provider
+export const TodoProvider = TodoContext.Provider;
